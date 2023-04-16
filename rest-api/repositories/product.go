b7e818e4500b2944683812ade755572595fc50f1
rayhankinan <rayhankinan@gmail.com>
2023-04-16 18:50:14 +0700

    Change some repositories

--- rest-api/repositories/product.go
+++ rest-api/repositories/product.go
@@ -1,23 +1,23 @@
 package repositories
 
 type SearchProductsQuery struct {
-	Search string `form:"search" field:"search" binding:"ascii"`
+	Search string `form:"search" field:"search"`
 	Page   int    `form:"page" field:"page" binding:"required,gt=0"`
 	Size   int    `form:"size" field:"size" binding:"required,gt=0"`
 }
 
 type AddProductRequest struct {
-	Name        string `json:"name" binding:"required,ascii"`
-	Image       string `json:"image" binding:"required,url"`
-	Description string `json:"description" binding:"required,ascii"`
+	Name        string `json:"name" binding:"required"`
+	Image       string `json:"image" binding:"required"`
+	Description string `json:"description" binding:"required"`
 	Price       uint   `json:"price" binding:"required,gte=0"`
 	Stock       uint   `json:"stock" binding:"required,gte=0"`
 }
 
 type EditProductRequest struct {
-	Name        string `json:"name" binding:"ascii"`
-	Image       string `json:"image" binding:"url"`
-	Description string `json:"description" binding:"ascii"`
+	Name        string `json:"name"`
+	Image       string `json:"image"`
+	Description string `json:"description"`
 	Price       uint   `json:"price" binding:"gte=0"`
 	Stock       uint   `json:"stock" binding:"gte=0"`
 }
--- rest-api/routes/product.go
+++ rest-api/routes/product.go
@@ -6,13 +6,14 @@ import (
 	"dagangan-product-rest-api/controllers"
 	"dagangan-product-rest-api/middlewares"
 	"dagangan-product-rest-api/types"
+	"dagangan-product-rest-api/utils/cache"
 )
 
 func ProductRoute(route *gin.Engine) {
 	productGroup := route.Group("/product")
 
-	productGroup.GET("/:id", middlewares.AuthMiddleware(types.Admin, types.Viewer), controllers.GetProductHandler())
-	productGroup.GET("/", middlewares.AuthMiddleware(types.Admin, types.Viewer), controllers.SearchProductHandler())
+	productGroup.GET("/:id", middlewares.AuthMiddleware(types.Admin, types.Viewer), cache.Store.GetHandlerFunc(controllers.GetProductHandler()))
+	productGroup.GET("/", middlewares.AuthMiddleware(types.Admin, types.Viewer), cache.Store.GetHandlerFunc(controllers.SearchProductHandler()))
 	productGroup.POST("/", middlewares.AuthMiddleware(types.Admin), controllers.AddProductHandler())
 	productGroup.PUT("/:id", middlewares.AuthMiddleware(types.Admin), controllers.EditProductHandler())
 	productGroup.DELETE("/:id", middlewares.AuthMiddleware(types.Admin), controllers.RemoveProductHandler())
